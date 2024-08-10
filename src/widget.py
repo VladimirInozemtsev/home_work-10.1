a023e9658ee0845bfe34a1924e4f1bb1034b3122
Vladimir Inozemtsev <vladimir.inozemtzev@yandex.ru>
2024-08-10 18:04:28 +0300

    linters ok

--- src/widget.py
+++ src/widget.py
@@ -1,27 +1,28 @@
 import masks
 
+
 def mask_account_card(data: str) -> str:
-  """
-  Функция принимает на вход строку, содержащую тип и номер карты или счета, и возвращает строку с замаскированным номером.
-  Args:
-      data: Строка с типом и номером карты или счета.
+    """
+    Функция принимает на вход строку, содержащую тип и номер карты или счета,
+    и возвращает строку с замаскированным номером.
+    Args:
+        data: Строка с типом и номером карты или счета.
 
-  Returns:
-      Строка с замаскированным номером.
-  """
+    Returns:
+        Строка с замаскированным номером.
+    """
 
-
-  if "Visa" in data or "Maestro" in data:
-    # Маскировка номера карты
-    masked_number = masks.get_mask_card_number(data.split()[-1])
-    masks.get_mask_card_number(data.split()[-1])
-    return data.split()[0] + " " + masked_number
-  elif "Счет" in data:
-    # Маскировка номера счета
-    masked_number = masks.get_mask_account(data.split()[-1])
-    return data.split()[0] + " " + masked_number
-  else:
-    return "Неверный формат данных"
+    if "Visa" in data or "Maestro" in data:
+        # Маскировка номера карты
+        masked_number = masks.get_mask_card_number(data.split()[-1])
+        masks.get_mask_card_number(data.split()[-1])
+        return data.split()[0] + " " + masked_number
+    elif "Счет" in data:
+        # Маскировка номера счета
+        masked_number = masks.get_mask_account(data.split()[-1])
+        return data.split()[0] + " " + masked_number
+    else:
+        return "Неверный формат данных"
 
 
 # print(mask_account_card("Visa Platinum 7000792289606361"))
@@ -31,20 +32,21 @@ def mask_account_card(data: str) -> str:
 
 
 def get_date(date_string: str) -> str:
-  """
-  Функция принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
-  и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024").
+    """
+    Функция принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024").
 
-  Args:
-      date_string: Строка с датой в формате "2024-03-11T02:26:18.671407".
+    Args:
+        date_string: Строка с датой в формате "2024-03-11T02:26:18.671407".
 
-  Returns:
-      Строка с датой в формате "ДД.ММ.ГГГГ".
-  """
-  year = date_string[:4]
-  month = date_string[5:7]
-  day = date_string[8:10]
-  return f"{day}.{month}.{year}"
+    Returns:
+        Строка с датой в формате "ДД.ММ.ГГГГ".
+    """
+    year = date_string[:4]
+    month = date_string[5:7]
+    day = date_string[8:10]
+    return f"{day}.{month}.{year}"
+
 
 # Пример использования:
 date_string = "2024-03-11T02:26:18.671407"
